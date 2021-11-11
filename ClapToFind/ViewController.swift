ddca3418ee98190118a00dde1917934f349d6b73
Polina Prokopenko <314kachu1337@gmail.com>
2021-11-11 16:40:44 +0300

    Initial Commit

---
+++ ClapToFind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClapToFind
+//
+//  Created by Polina Prokopenko on 11/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
