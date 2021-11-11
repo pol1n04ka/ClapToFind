deb2afd39657ecc58d0bd30cd6b00c8eb16cbfe7
Polina Prokopenko <314kachu1337@gmail.com>
2021-11-11 22:49:30 +0300

    Added TensorflowLite and model for reconize claps

---
+++ ClapToFind/SoundRecognition/AudioInputManager.swift
@@ -0,0 +1,8 @@
+//
+//  AudioInputManager.swift
+//  ClapToFind
+//
+//  Created by Polina Prokopenko on 11/11/21.
+//
+
+import Foundation
--- ClapToFind/SoundRecognition/SoundRecognition.swift
+++ ClapToFind/SoundRecognition/SoundRecognition.swift
@@ -12,4 +12,5 @@ class SoundRecognition {
     
     // Code for sound recognition
     
+    
 }
