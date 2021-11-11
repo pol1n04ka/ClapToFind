5b8af076ff359d51eb17ffcd3722a5fef3b21f58
Polina Prokopenko <314kachu1337@gmail.com>
2021-11-11 16:43:56 +0300

    Base project

---
+++ ClapToFind/SoundRecognition.swift
@@ -0,0 +1,16 @@
+//
+//  SoundRecognition.swift
+//  ClapToFind
+//
+//  Created by Polina Prokopenko on 11/11/21.
+//
+
+import Foundation
+import SoundAnalysis
+
+
+class SoundRecognition {
+    
+    // Code for sound recognition
+    
+}
